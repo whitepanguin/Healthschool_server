b6cf4edc10efc9c1d2f76dd9370fd97db1c3235e
smh070519 <minsmh0815@gmail.com>
2025-01-18 18:50:56 +0900

    1대1문의

--- controller/help/helpcontroller.js
+++ controller/help/helpcontroller.js
@@ -1,5 +1,6 @@
 import PopularQuesetion from "../../models/help/popularQuesetionSchema.js"
 import notice from "../../models/help/notice.js";
+import PostChat from "../../models/help/postChat.js";
 import { create } from "domain";
 
 export const getPopularQuestion = async (req, res) => {
@@ -25,6 +26,24 @@ export const getNotice = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 }
+
+
+export const postChat = async (req, res) => {
+  const { title, email,content } = req.body;
+
+  try {
+    await PostChat.create({
+      title : req.boy.title,
+      email: req.body.email,
+      content : req.body.content
+    });
+    const saveChat = await newChat.save();
+    res.status(201).json(saveChat); // 동영상 추가 후 응답
+  } catch (err) {
+    console.error("Error saving store:", err);
+    res.status(500).json({ error: "Failed to add chat" });
+  }
+};
 
 
 
--- routes/help/helpRouter.js
+++ routes/help/helpRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {  getNotice, getPopularQuestion } from '../../controller/help/helpcontroller.js';
+import {  getNotice, getPopularQuestion,postChat } from '../../controller/help/helpcontroller.js';
 
 const helpRouter = express.Router()
 
 helpRouter.get("/getPopularQuesetion", getPopularQuestion)
 helpRouter.get("/getNotice", getNotice)
+helpRouter.post("/postchat", postChat)
 
 export default helpRouter;
