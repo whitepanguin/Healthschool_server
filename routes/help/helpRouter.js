f91779da3b390ff1d6e7ae9b18ff9068c0aaa5f2
smh070519 <minsmh0815@gmail.com>
2025-01-22 17:17:01 +0900

    1대1문의 페이지

--- routes/help/helpRouter.js
+++ routes/help/helpRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {  getNotice, getPopularQuestion,postChat } from '../../controller/help/helpcontroller.js';
+import {  getNotice, getPopularQuestion,getPostChat,postChat } from '../../controller/help/helpcontroller.js';
 
 const helpRouter = express.Router()
 
 helpRouter.get("/getPopularQuesetion", getPopularQuestion)
 helpRouter.get("/getNotice", getNotice)
 helpRouter.post("/postchat", postChat)
+helpRouter.post("/getPostChat", getPostChat)
 
 export default helpRouter;
