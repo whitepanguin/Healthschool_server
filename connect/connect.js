ad30529925349a1623b574b86dcb8621be776552
whiteraaccoon <jyl04046@gmail.com>
2025-04-01 17:24:03 +0900

    fix?

--- connect/connect.js
+++ connect/connect.js
@@ -1,24 +1,23 @@
 import mongoose from "mongoose";
 
-const connection_url = `mongodb+srv://app:1234@app.lixyw.mongodb.net/`;
+const connection_url = `mongodb+srv://admin:admin@app.lixyw.mongodb.net/?retryWrites=true&w=majority&appName=app`;
 
 const connect = () => {
-  if(process.env.NODE_ENV != "production"){
-    mongoose.set("debug", true)
+  if (process.env.NODE_ENV != "production") {
+    mongoose.set("debug", true);
   }
 
   mongoose
     .connect(connection_url, {
-      dbName : "app" // 프로젝트 이름
+      dbName: "app", // 프로젝트 이름
     })
     .then(() => {
-      console.log("Connection to MongoDB") // 연결 성공
+      console.log("Connection to MongoDB"); // 연결 성공
     })
     .catch((err) => {
-      console.error("Connected fail to MongDB") // 연결 실패
-      console.log(err)
-    })
-
-}
+      console.error("Connected fail to MongDB"); // 연결 실패
+      console.log(err);
+    });
+};
 
 export default connect;
